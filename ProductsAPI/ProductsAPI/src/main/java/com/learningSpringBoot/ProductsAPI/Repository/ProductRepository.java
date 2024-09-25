157808559f9b14e75c323b9318c83e253d533afc
Nicolás <nicolas20032401@gmail.com>
2024-09-25 20:02:41 -0300

    Delete ProductsAPI directory
    I forgot to make something in git bash hehe.

--- ProductsAPI/ProductsAPI/src/main/java/com/learningSpringBoot/ProductsAPI/Repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.learningSpringBoot.ProductsAPI.Repository;
-
-import com.learningSpringBoot.ProductsAPI.Model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductRepository extends JpaRepository<Product, Integer> {
-}
